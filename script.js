307d821ded5836a6c9a4dc75912bd0a45f045c94
Dan Lin <dlin2102@gmail.com>
2017-01-30 22:16:21 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  var score = 0;
+
+  for (var i=1; i < 5; i++) {
+    setQuizListeners('question' + i, 'question' + (parseInt(i)+1));
+  }
+  
+  $('input:radio[name=question5]').on('click', function(){
+    tally();
+  })
+
+
+  function tally(){
+    var choice1 = $('input:radio[name=question1]:checked').val();
+    var choice2 = $('input:radio[name=question2]:checked').val();
+    var choice3 = $('input:radio[name=question3]:checked').val();
+    var choice4 = $('input:radio[name=question4]:checked').val();
+    var choice5 = $('input:radio[name=question5]:checked').val();
+    if(choice1 === "Kill him but only with her bare hands"){
+      score++;
+    }
+    if(choice2 === "Muhammad"){
+      score++;
+    }
+    if(choice3 === "Clocks"){
+      score++;
+    }
+    if(choice4 === "An unkindness"){
+      score++;
+    }
+    if(choice5 === "5 months"){
+      score++;
+    }
+
+    $('#scoreboard').text(score);
+  }
+
+  function setQuizListeners(currentInputName, nextInputName) {
+    $('input:radio[name= ' +currentInputName+ ']').on('click', function(){
+      $('.' + nextInputName).show();
+      $('.' + currentInputName).hide();
+    })
+  }
+
+
+
+
 });
